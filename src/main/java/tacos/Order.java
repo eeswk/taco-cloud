7237145a520df26ad1166c7560704582484d7b67
themorning <themorning84@gmail.com>
2020-08-18 02:39:11 +0900

    JPA로 변경으로 CrudRepository를 상속받음

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,8 +1,10 @@
 package tacos;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -10,7 +12,13 @@ import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
 @Data
-public class Order {
+@Entity
+@Table(name = "Taco_Order")
+public class Order implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Date placedAt;
 
@@ -31,8 +39,14 @@ public class Order {
     @Digits(integer = 3, fraction = 0, message = "Invalid CW")
     private String ccCW;
 
+    @ManyToMany(targetEntity = Taco.class)
     private List<Taco> tacos = new ArrayList<>();
     public void addDesign(Taco design) {
         this.tacos.add(design);
+    }
+
+    @PrePersist
+    void placedAt() {
+        this.placedAt = new Date();
     }
 }
--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -2,18 +2,34 @@ package tacos;
 
 import java.util.Date;
 import java.util.List;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.Persistent;
 
 @Data
+@RequiredArgsConstructor
+@Entity
 public class Taco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Date createdAt;
 
     @NotNull
     @Size(min=5, message = "Name must be ato least 5 characters long")
     private String name;
+
+    @ManyToMany(targetEntity = Ingredient.class)
     @Size(min=1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+    }
 }
